c2fd1c2feaa3959f5d4abc89097a7538f293ed8c
seokgyo <seokgyo@gmail.com>
2016-03-18 12:12:33 +0900

    signin.

---
+++ containers/Signin.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import React, {
+  StyleSheet,
+  Text,
+  TextInput,
+  View
+} from 'react-native';
+import { connect } from 'react-redux'
+import { authActions } from 'goommerce-redux';
+
+const Signin = React.createClass({
+  getInitialState() {
+    return {};
+  },
+  signin() {
+    const { email, password } = this.state;
+    this.props.login(email, password).then((res) => console.log(res));
+  },
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType='email-address'
+          onChangeText={(email) => this.setState({ email })}
+          placeholder='Email address'
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          value={this.state.email}
+        />
+        <TextInput
+          autoCapitalize='none'
+          autoCorrect={false}
+          onChangeText={(password) => this.setState({ password })}
+          placeholder='Password'
+          secureTextEntry={true}
+          style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+          value={this.state.password}
+        />
+        <Text style={styles.signin} onPress={this.signin}>
+          Sign In
+        </Text>
+      </View>
+    );
+  }
+});
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  signin: {
+    borderColor: '#00ff00',
+    borderWidth: 2,
+    color: '#333333',
+    textAlign: 'center',
+  },
+});
+
+export default connect(undefined, authActions)(Signin);
--- index.ios.js
+++ index.ios.js
@@ -7,60 +7,25 @@ import React, {
   AppRegistry,
   Component,
   StyleSheet,
-  Text,
-  View
 } from 'react-native';
 
-import { Provider, connect } from 'react-redux'
+import { Provider } from 'react-redux'
 import { config as configApiClient } from 'goommerce-api-client';
-import configureStore, { authActions } from 'goommerce-redux';
+import configureStore from 'goommerce-redux';
+
+import Signin from './containers/Signin';
 
 configApiClient({ apiRoot: 'http://localhost:8080'})
 const store = configureStore();
-
-class Signin extends Component {
-  componentDidMount() {
-    this.props.login('ss@gmail.com', 'ss').then((res) => console.log(res));
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.instructions}>
-         {this.props.auth.email}
-        </Text>
-      </View>
-    );
-  }
-}
-const SigninWrap = connect((state) => ({ auth: state.auth }), authActions)(Signin);
 
 class GoommerceSeller extends Component {
   render() {
     return (
       <Provider store={store}>
-        <SigninWrap />
+        <Signin />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('GoommerceSeller', () => GoommerceSeller);
