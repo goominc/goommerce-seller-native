12a41d049fe1d52ed2a1cc7ed6937eb4328e0476
seokgyo <seokgyo@gmail.com>
2016-03-24 02:17:44 +0900

    show product image in order item.

--- components/OrderItem.js
+++ components/OrderItem.js
@@ -1,22 +1,33 @@
 'use strict';
 
 import React, {
+  Image,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+const _ = require('lodash');
+
 export default React.createClass({
   render() {
     const { order } = this.props;
+    const { product, productVariant } = order;
     return (
       <View>
         <Text>id: {order.id}</Text>
+        <Text>nick: {product.data.nickname.ko}</Text>
+        <Text>color: {productVariant.data.color}</Text>
+        <Text>size: {productVariant.data.size}</Text>
         <Text>Unit Price(KRW): {order.KRW}</Text>
         <Text>Count: {order.orderedCount}</Text>
         <Text>Total(KRW): {order.totalKRW}</Text>
         <Text>Total(USD): {order.totalUSD}</Text>
+        <Image
+          style={styles.image}
+          source={{ uri: `https://${_.get(productVariant.appImages.default, '0.url')}` }}
+        />
         <Icon.Button name="check">
           <Text style={styles.signin}>In Stock</Text>
         </Icon.Button>
@@ -29,4 +40,8 @@ export default React.createClass({
 });
 
 const styles = StyleSheet.create({
+  image: {
+    width: 90,
+    height: 90,
+  },
 });
