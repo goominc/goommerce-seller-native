ee74adc6625cf4605c8cfe7760cce859a6e3e9cf
seokgyo <seokgyo@gmail.com>
2016-04-14 16:17:19 +0900

    show alertbox if app is active.

--- GoommerceSeller.js
+++ GoommerceSeller.js
@@ -13,10 +13,18 @@ import { Provider } from 'react-redux'
 import { config as configApiClient } from 'goommerce-api-client';
 import configureStore, { errorActions } from 'goommerce-redux';
 import { cloudinaryConfig } from 'react-cloudinary';
+import OneSignal from 'react-native-onesignal';
 
 import App from './containers/App';
 
 cloudinaryConfig({ cloud_name: 'linkshops', crop: 'limit' });
+OneSignal.configure({
+  onNotificationOpened(message, data, isActive) {
+    if (isActive) {
+      Alert.alert('New Order!!!', message);
+    }
+  }
+});
 
 if (__DEV__) {
   configApiClient({ apiRoot: (Platform.OS === 'ios') ? 'http://localhost:8080' : 'http://10.0.3.2:8080' });
