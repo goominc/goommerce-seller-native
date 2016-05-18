a8a18fdee5ee2adccd5fc14dba20a7c9d374b1c6
seokgyo <seokgyo@gmail.com>
2016-05-18 17:46:45 +0900

    android toolbar

--- containers/App.android.js
+++ containers/App.android.js
@@ -2,12 +2,18 @@
 
 import React, {
   BackAndroid,
+  Dimensions,
+  DrawerLayoutAndroid,
   Navigator,
+  StatusBar,
   StyleSheet,
   View
 } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import routes from '../routes';
+
+const DRAWER_WIDTH_LEFT = 56;
 
 // FIXME: Uses NavigatorExperimental when it's ready.
 let _navigator;
@@ -20,21 +26,60 @@ BackAndroid.addEventListener('hardwareBackPress', () => {
 });
 
 export default React.createClass({
+  componentWillMount() {
+    BackAndroid.addEventListener('hardwareBackPress', this.handleBackButtonPress);
+  },
+  handleBackButtonPress() {
+    if (this._overrideBackPressForDrawerLayout) {
+      // This hack is necessary because drawer layout provides an imperative API
+      // with open and close methods. This code would be cleaner if the drawer
+      // layout provided an `isOpen` prop and allowed us to pass a `onDrawerClose` handler.
+      this.drawer && this.drawer.closeDrawer();
+      return true;
+    }
+    return false;
+  },
   renderScene(route, navigator) {
     _navigator = navigator;
     return (
       <View style={styles.scene}>
+        <MaterialIcons.ToolbarAndroid
+          navIconName="menu"
+          onIconClicked={() => this.drawer.openDrawer()}
+          style={styles.toolbar}
+          title={route.title}
+        />
         <route.component {...route.props} push={navigator.push} />
+      </View>
+    );
+  },
+  renderDrawerContent() {
+    return (
+      <View style={styles.drawerContentWrapper}>
       </View>
     );
   },
   render() {
     return (
-      <Navigator
-        initialRoute={routes.home()}
-        renderScene={this.renderScene}
-        style={styles.container}
-      />
+      <DrawerLayoutAndroid
+        drawerPosition={DrawerLayoutAndroid.positions.Left}
+        drawerWidth={Dimensions.get('window').width - DRAWER_WIDTH_LEFT}
+        keyboardDismissMode="on-drag"
+        onDrawerOpen={() => {
+          this._overrideBackPressForDrawerLayout = true;
+        }}
+        onDrawerClose={() => {
+          this._overrideBackPressForDrawerLayout = false;
+        }}
+        ref={(drawer) => { this.drawer = drawer; }}
+        renderNavigationView={this.renderDrawerContent}
+        statusBarBackgroundColor="#589c90">
+        <Navigator
+          initialRoute={routes.home()}
+          renderScene={this.renderScene}
+          style={styles.container}
+        />
+      </DrawerLayoutAndroid>
     );
   }
 });
@@ -44,8 +89,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
+  toolbar: {
+    backgroundColor: '#e9eaed',
+    height: 56,
+  },
   scene: {
     flex: 1,
+    backgroundColor: 'white',
+  },
+  drawerContentWrapper: {
+    flex: 1,
+    paddingTop: StatusBar.currentHeight,
     backgroundColor: 'white',
   },
 });
