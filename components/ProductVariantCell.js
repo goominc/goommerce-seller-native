026a1e0f6bbc43b78c136064081dd77ef3811330
seokgyo <seokgyo@gmail.com>
2016-07-05 18:26:31 +0900

    handle no image.

--- components/ProductVariantCell.js
+++ components/ProductVariantCell.js
@@ -4,24 +4,38 @@ import React from 'react';
 import { Image, Picker, StyleSheet, Text, View } from 'react-native';
 import { CloudinaryImageNative } from 'react-cloudinary/lib/native';
 import numeral from 'numeral';
+import _ from 'lodash';
 
 import DefaultText from './DefaultText';
 import OpenUrlButton from './OpenUrlButton';
 
 export default React.createClass({
+  renderThumbnail() {
+    const image = _.get(this.props.productVariant, 'appImages.default.0', {});
+    if (image.publicId) {
+      return (
+        <CloudinaryImageNative
+          publicId={image.publicId}
+          options={{ width: 200, height: 200 }}
+          style={styles.thumbnail}
+        />
+      );
+    } else {
+      return (
+        <View style={[styles.thumbnail, { alignItems: 'center', justifyContent: 'center' }]}>
+          <Text style={{ fontSize: 11, color: '#999999' }}>No Image</Text>
+        </View>
+      );
+    }
+  },
   render() {
     const { product, productVariant } = this.props;
-    const image = productVariant.appImages.default[0];
     const { color, size } = productVariant.data;
     const KRW = productVariant.KRW || product.KRW;
     return (
       <View style={styles.container}>
         <OpenUrlButton url={`https://m.linkshops.com/products/${product.id}`}>
-          <CloudinaryImageNative
-            publicId={image.publicId}
-            options={{ width: 200, height: 200 }}
-            style={styles.thumbnail}
-          />
+          {this.renderThumbnail()}
         </OpenUrlButton>
         <View style={styles.descContainer}>
           <Text style={{ fontSize: 14, color: '#4c4c4c' }}>{color}</Text>
